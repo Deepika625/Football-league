4c6a9aacd510ee5775111102ed4528250f51ce3d
Deepika625 <118673564+Deepika625@users.noreply.github.com>
2022-12-18 00:23:50 +0530

    Football League

---
+++ HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Football_League.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Football_League.Controllers
+{
+    public class HomeController : Controller
+    {
+        MyDbContext context = new MyDbContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(context.matches);
+        }
+    }
+}
---
+++ MyDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web.Configuration;
+using System.Data.Entity;
+
+namespace Football_League.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() : base("name=conn")
+        {
+
+        }
+
+        public DbSet<match> matches { get; set; }
+    }
+}
+      
+        
+      
+
+
+    
+
+    
